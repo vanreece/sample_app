6db2234e9e9d4913ef1bda18efc60020b957e492
Danny Krause <danny@carbonfive.com>
2011-12-20 11:51:34 -0800

    11.1.2: User/Micropost associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
 
+  has_many :microposts
+
   validates :name,
             presence: true,
             length: { maximum: 50 }
