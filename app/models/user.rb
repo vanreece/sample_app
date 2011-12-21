331cd2e631acde2993f4c5e6b79e4c8a3ec38770
Danny Krause <danny@carbonfive.com>
2011-12-21 15:03:21 -0800

    12.1.5 Followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ActiveRecord::Base
                            dependent: :destroy
   has_many :following, through: :relationships, source: :followed
 
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name: "Relationship",
+                                   dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   validates :name,
             presence: true,
             length: { maximum: 50 }
